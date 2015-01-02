df69089ffd64f2956045521bf29ef02e0a3eacef
Andy <stannarda@gmail.com>
2015-01-02 07:59:32 +0000

    first commit of Lions and Tigers

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Andy on 31/12/2014.
+//  Copyright (c) 2014 Andy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var breed = ""
+    var name = ""
+    var image = UIImage(named:"")
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var age: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var breed: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myTiger = Tiger()
+        myTiger.name = "Boris"
+        myTiger.breed = "Tabby"
+        myTiger.age = 13
+        myTiger.image = UIImage(named: "Lioness.jpeg")
+        
+        println("My tigers name is: \(myTiger.name) its' age is \(myTiger.age)")
+        
+        myImageView.image = myTiger.image
+        name.text = myTiger.name
+        age.text = "\(myTiger.age)"
+        breed.text = myTiger.breed
+        
+        var secondTiger = Tiger()
+        secondTiger.name = "Tigeress"
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.age = 2
+        secondTiger.image = UIImage(named: "IndochineseTiger.jpg")
+        
+        var thirdTiger = Tiger()
+        thirdTiger.name = "Jack"
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.age = 4
+        thirdTiger.image = UIImage(named: "MalayanTiget.jpg")
+        
+        var fourthTiger = Tiger()
+        fourthTiger.name = "Betty"
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.age = 5
+        fourthTiger.image = UIImage(named: "SiberianTiger.jpg")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +58,8 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func NextButtonPressed(sender: UIBarButtonItem) {
+        
+    }
 }
 
